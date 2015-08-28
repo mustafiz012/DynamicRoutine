e4e5899690154dab0f51d6b48abcbaa76e8577aa
mustafiz012 <mustafiz_kuet_cse@yahoo.com>
2015-08-29 03:17:53 +0600

    MainActivity Play button test

--- app/src/main/java/routine/kuet/musta/MainActivity.java
+++ app/src/main/java/routine/kuet/musta/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -58,7 +59,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 		else if (v == thursday)
 			startActivity(new Intent(this, Thursday.class));
 		else if(v == play){
-
+			Toast.makeText(MainActivity.this, "This will play my favourite song!!", Toast.LENGTH_SHORT).show();
 		}
 	}
 }
