a7f08122738bbbd8a65e294b0f60a4880747b6e3
mustafiz012 <mustafiz_kuet_cse@yahoo.com>
2015-08-28 19:36:53 +0600

    this is first commit...addition and deletion work nicely

---
+++ app/src/main/java/routine/kuet/musta/MainActivity.java
@@ -0,0 +1,64 @@
+package routine.kuet.musta;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+	Button saturday, sunday, monday, tuesday, wednesday, thursday, play;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initializer();
+	}
+
+	public void initializer(){
+		saturday = (Button) findViewById(R.id.saturday);
+		sunday = (Button) findViewById(R.id.sunday);
+		monday = (Button) findViewById(R.id.monday);
+		tuesday = (Button) findViewById(R.id.tuesday);
+		wednesday = (Button) findViewById(R.id.wednesday);
+		thursday = (Button) findViewById(R.id.thursday);
+		play = (Button) findViewById(R.id.song);
+		saturday.setOnClickListener(this);
+		sunday.setOnClickListener(this);
+		monday.setOnClickListener(this);
+		tuesday.setOnClickListener(this);
+		wednesday.setOnClickListener(this);
+		thursday.setOnClickListener(this);
+		play.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == saturday)
+			startActivity(new Intent(this, Saturday.class));
+		else if (v == sunday)
+			startActivity(new Intent(this, Sunday.class));
+		else if (v == monday)
+			startActivity(new Intent(this, Monday.class));
+		else if (v == tuesday)
+			startActivity(new Intent(this, Tuesday.class));
+		else if (v == wednesday)
+			startActivity(new Intent(this, Wednesday.class));
+		else if (v == thursday)
+			startActivity(new Intent(this, Thursday.class));
+		else if(v == play){
+
+		}
+	}
+}
---
+++ app/src/main/java/routine/kuet/musta/Sunday.java
@@ -0,0 +1,160 @@
+package routine.kuet.musta;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Map;
+
+public class Sunday extends ActionBarActivity implements View.OnClickListener {
+
+	Button createCourses, deleteCourses, home;
+	LinearLayout showCourse;
+	TextView[] tvName;
+	int counter =0;
+	SharedPreferences spCreateName;
+	SharedPreferences.Editor editor, eCreateName;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_saturday);
+		initialization();
+	}
+
+	public void initialization(){
+		spCreateName = getSharedPreferences("MyCourses", 0);
+		eCreateName = spCreateName.edit();
+		createCourses = (Button) findViewById(R.id.createCourse);
+		deleteCourses = (Button) findViewById(R.id.deleteOne);
+		home  = (Button) findViewById(R.id.home);
+		showCourse = (LinearLayout) findViewById(R.id.showCourses);
+		createCourses.setOnClickListener(this);
+		deleteCourses.setOnClickListener(this);
+		home.setOnClickListener(this);
+		courseViewer();
+		tvName = new TextView[100];
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == createCourses){
+			counter++;
+			final String count = String.valueOf(counter);
+			AlertDialog.Builder createBuilder = new AlertDialog.Builder(this);
+			createBuilder.setTitle("Name Your Course");
+			final EditText courseName = new EditText(this);
+			courseName.setHint("Name & Time & Teacher Name");
+			if (courseName != null)
+				createBuilder.setView(courseName);
+			createBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					final String cName = courseName.getText().toString();
+					if (cName.isEmpty()) {
+						Toast.makeText(Sunday.this, "Don't make it blank!!", Toast.LENGTH_SHORT).show();
+					} else {
+						//counter++;
+						Toast.makeText(Sunday.this, "Courses Added: " + cName, Toast.LENGTH_SHORT).show();
+						eCreateName.putString(cName, cName);
+						eCreateName.apply();
+						courseViewer();
+						eCreateName.commit();
+					}
+				}
+			});
+			createBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+				}
+			});
+			AlertDialog alertDialog = createBuilder.create();
+			alertDialog.show();
+		}
+		else if(v == deleteCourses){
+			final AlertDialog.Builder deleteCourse = new AlertDialog.Builder(this);
+			deleteCourse.setTitle("Delete Course");
+			final EditText courseName = new EditText(this);
+			final int cID = courseName.getId();
+			courseName.setHint("which one U wanna delete:");
+			Map<String, ?> addedCourses = spCreateName.getAll();
+			int i =0;
+			for (Map.Entry<String, ?> entry : addedCourses.entrySet()) {
+				final String deleteAddedCourses = entry.getKey();
+				i++;
+				TextView hints = new TextView(this);
+				hints.setText(deleteAddedCourses);
+				deleteCourse.setMessage("Select : "+cID);
+			}
+			deleteCourse.setView(courseName);
+			deleteCourse.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					final String cName = courseName.getText().toString();
+					if (cName.isEmpty()) {
+						Toast.makeText(Sunday.this, "Nothing To Delete!", Toast.LENGTH_SHORT).show();
+					} else {
+						eCreateName.remove(cName);
+						eCreateName.apply();
+						eCreateName.apply();
+						courseViewer();
+					}
+				}
+			});
+			deleteCourse.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+				}
+			});
+			AlertDialog test = deleteCourse.create();
+			test.show();
+		}
+		else if(v == home)
+			startActivity(new Intent(this, MainActivity.class));
+	}
+	@SuppressLint("NewApi")
+	public void courseViewer() {
+
+		try {
+			showCourse.removeAllViews();
+		} catch (Exception ignored)
+		{ }
+		Map<String, ?> allName = spCreateName.getAll();
+		int i = 0;
+		for (Map.Entry<String, ?> entry : allName.entrySet()) {
+			final String courseName = entry.getKey();
+			i++;
+			//Toast.makeText(Saturday.this, courseName, Toast.LENGTH_SHORT).show();
+			TextView rowTextView = new TextView(this);
+			rowTextView.setGravity(Gravity.CENTER);
+			//rowTextView.setAllCaps(true);
+			rowTextView.setText(i + ". " + courseName);
+			rowTextView.setTextSize(20);
+			rowTextView.setBackgroundDrawable(getDrawable(R.drawable.custom_bg_22));
+			rowTextView.setId(i);
+			rowTextView.setPadding(5, 5, 5, 5);
+			rowTextView.setTextColor(getResources().getColor(R.color.button_material_dark));
+			rowTextView.setTextIsSelectable(true);
+			rowTextView.setCursorVisible(deleteDatabase(courseName));
+			rowTextView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Toast.makeText(Sunday.this, "This is "+courseName, Toast.LENGTH_SHORT).show();
+				}
+			});
+			showCourse.addView(rowTextView);
+		}
+	}
+}
